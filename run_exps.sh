3ff5fa4b4ffe0089a1e2e14e72e3f6aa7fedf177
kbkbowo <b09901073@ntu.edu.tw>
2024-01-03 06:32:21 +0800

    code

---
+++ run_exps.sh
@@ -0,0 +1,4 @@
+accelerate launch train.py --cfg_name base
+accelerate launch train.py --cfg_name norm
+accelerate launch train.py --cfg_name coord
+accelerate launch train.py --cfg_name normAndCoord
